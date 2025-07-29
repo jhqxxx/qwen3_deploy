e7609daf63184af059d88449d37a73a5ddfa413f
wxg <1584929962@qq.com>
2025-07-29 13:44:38 +0800

    fix api

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,32 @@
 use openai_dive::v1::resources::chat::ChatCompletionChunkResponse;
 use qwen3_deploy::chat_stream;
+use rocket::Request;
 use rocket::futures::{Stream, StreamExt};
 use rocket::http::ContentType;
+use rocket::response::Responder;
 use rocket::response::stream::TextStream;
 use rocket::serde::json::Json;
 
+enum Response<R: Stream<Item = String> + Send> {
+    Stream(TextStream<R>),
+    Text(String),
+}
+
+impl<'r, 'o: 'r, R> Responder<'r, 'o> for Response<R>
+where
+    R: Stream<Item = String> + Send + 'o,
+    'r: 'o,
+{
+    fn respond_to(self, req: &'r Request<'_>) -> rocket::response::Result<'o> {
+        match self {
+            Response::Stream(stream) => stream.respond_to(req),
+            Response::Text(text) => text.respond_to(req),
+        }
+    }
+}
+// 为 Response 枚举实现 Responder trait
 #[post("/completions", data = "<req>")]
-pub(crate) async fn chat(req: String) -> (ContentType, TextStream<impl Stream<Item = String>>) {
+pub(crate) async fn chat(req: String) -> (ContentType, Response<impl Stream<Item = String>>) {
     let stream = TextStream! {
         match chat_stream(&req) {
             Ok(stream) => {
@@ -21,5 +41,5 @@ pub(crate) async fn chat(req: String) -> (ContentType, TextStream<impl Stream<It
             }
         }
     };
-    (ContentType::EventStream, stream)
+    (ContentType::EventStream, Response::Stream(stream))
 }
